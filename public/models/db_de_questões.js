cf6206d52feb3765511615d1e716e07f251759cc
Henrique de Oliveira Ramos <henriqueramos.qs@gmail.com>
2023-01-26 13:39:35 -0300

    add: database backup

--- public/models/db_de_questões.js
+++ public/models/db_de_questões.js
@@ -3,10 +3,10 @@ const mysql = require('mysql')
 
 var db  = mysql.createPool({
     connectionLimit : 10,
-    host            : '<host>',
-    user            : '<user>',
-    password        : '<password>',
-    database        : '<database>'
+    host            : 'localhost',
+    user            : 'root',
+    password        : 'megareaper3107',
+    database        : 'questoes'
   });
 
 module.exports = {
